45f90585fa1bd1c8a10e4502f6afa08742b728ea
zaidanammar <zaidanammar23@gmail.com>
2023-01-31 17:36:00 +0700

    refactor: animation onChange pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,13 @@
 import type { AppProps } from 'next/app'
 import { Suspense, useState } from 'react'
-import { AnimatePresence } from 'framer-motion'
 import withRedux from 'next-redux-wrapper'
 import { PersistGate } from 'redux-persist/integration/react'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { store, persistor } from 'store/index'
 import Layout from '@/organisms/layout'
-import Transition from '@/atoms/animations/transition'
-import '../styles/globals.css'
 import type { NextPageWithLayout } from '@/types/index'
+import '../styles/globals.css'
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
@@ -29,13 +27,9 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <QueryClientProvider client={queryClient}>
       <PersistGate persistor={persistor}>
-        <AnimatePresence initial={false}>
-          <Transition>
-            {getLayout(<Component {...pageProps} />)}
-            <ReactQueryDevtools />
-          </Transition>
-        </AnimatePresence>
+        {getLayout(<Component {...pageProps} />)}
       </PersistGate>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
--- src/components/organisms/layout/index.tsx
+++ src/components/organisms/layout/index.tsx
@@ -1,9 +1,30 @@
 import React, { ReactNode, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { ProSidebarProvider } from 'react-pro-sidebar'
+import { motion, AnimatePresence } from 'framer-motion'
 import RightBar from '@/templates/rightbar'
 import Sidebar from '@/templates/sidebar'
 import { useAppSelector } from 'store/hooks'
+
+const variants = {
+  in: {
+    opacity: 1,
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.25,
+    },
+  },
+  out: {
+    opacity: 0,
+    scale: 1,
+    y: 40,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}
 
 type OrganismsLayoutProps = {
   children: ReactNode
@@ -36,7 +57,18 @@ const OrganismsLayout = ({ children }: OrganismsLayoutProps) => {
         <section className="flex h-full w-full gap-4">
           <Sidebar />
           <div className="flex-1 overflow-y-scroll rounded-2xl bg-neutral-10 px-6 py-10">
-            {children}
+            <AnimatePresence initial>
+              <motion.div
+                key={`an${router.asPath}`}
+                variants={variants}
+                animate="in"
+                initial="out"
+                exit="out"
+                className="h-full w-full"
+              >
+                {children}
+              </motion.div>
+            </AnimatePresence>
           </div>
           <RightBar />
         </section>
