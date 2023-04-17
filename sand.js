7e14052e815569d42041c66ba5d1e22ec4867111
zaidanammar <zaidanammar23@gmail.com>
2023-04-17 15:25:28 +0700

    feat(auth): add logout modal :sparkles: :zap:

---
+++ sand.js
@@ -0,0 +1,269 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const access = [
+	{
+		uuid: 'da2733ba-1518-4f7a-ae35-d832d77a7882',
+		name: 'Create Restaurant',
+		key: 'restaurant:create',
+		description: 'Can create a restaurant data',
+		is_internal: false,
+	},
+	{
+		uuid: 'da2733ba-1518-4f7a-ae35-1431315153135',
+		name: 'Create User',
+		key: 'user:create',
+		description: 'Can create a user data',
+		is_internal: false,
+	},
+	{
+		uuid: 'da2733ba-1518-4f7a-ae35-d832d77a7882',
+		name: 'Create Restaurant',
+		key: 'restaurant:update',
+		description: 'Can update a restaurant data',
+		is_internal: false,
+	},
+	{
+		uuid: 'da2733ba-1518-4f7a-ae35-d832d77a7882',
+		name: 'Create Restaurant',
+		key: 'restaurant:delete',
+		description: 'Can update a restaurant data',
+		is_internal: false,
+	},
+	{
+		uuid: 'ed3ef9d2-52b8-4453-bf94-0712591f91a5',
+		name: 'View Subscription History',
+		key: 'subscription:history',
+		description: 'Can view list of all subscription history',
+		is_internal: false,
+	},
+	{
+		uuid: '8349239e-5d86-4f2f-b1c1-3e5f2207c765',
+		name: 'Assign Subscription',
+		key: 'subscription:assign',
+		description: 'Can assign a subscripton to restaurant',
+		is_internal: false,
+	},
+	{
+		uuid: 'd8a3a22e-1df8-4df7-98b7-ee933d935791',
+		name: 'Create Subscription',
+		key: 'subscription:create',
+		description: 'Can create a subscripton data',
+		is_internal: false,
+	},
+	{
+		uuid: '21969c15-0481-4162-9407-be4d28a6669f',
+		name: 'Delete Subscription',
+		key: 'subscription:delete',
+		description: 'Can delete a subscripton data',
+		is_internal: false,
+	},
+	{
+		uuid: 'e764d3cb-ef52-49a8-95b9-beb72750f462',
+		name: 'View Subscription',
+		key: 'subscription:read',
+		description: 'Can view list of all subscriptions',
+		is_internal: false,
+	},
+	{
+		uuid: 'c9b2dcca-6638-4409-bddc-56afe2e240a6',
+		name: 'Edit Subscription',
+		key: 'subscription:update',
+		description: 'Can update a subscripton data',
+		is_internal: false,
+	},
+	{
+		uuid: '402d2095-e461-4898-bd87-0b198bdaaf8b',
+		name: 'Edit Admin Role',
+		key: 'admin:update_role',
+		description: 'Can update role of admin',
+		is_internal: false,
+	},
+	{
+		uuid: '7478bfdc-6454-411a-b8a2-41e05bfddf48',
+		name: 'View Admin List',
+		key: 'admin:read',
+		description: 'Can view list of all administrator Users',
+		is_internal: false,
+	},
+	{
+		uuid: 'df2e4ce7-5b22-479c-a7e1-f09cfa4112c1',
+		name: 'Delete Admin',
+		key: 'admin:delete',
+		description: 'Can delete an Administrator account.',
+		is_internal: false,
+	},
+	{
+		uuid: '26ce08be-2e70-4f0a-8c44-fb13a1c795b8',
+		name: 'Edit Admin',
+		key: 'admin:update',
+		description: 'Can promote and demote Administrator Account',
+		is_internal: false,
+	},
+	{
+		uuid: '8b7e25d8-5155-4b7a-8630-b079039984fe',
+		name: 'Create Admin',
+		key: 'admin:create',
+		description: 'Can create an administrator account',
+		is_internal: false,
+	},
+	{
+		uuid: 'b5f9f26d-f145-4b30-8198-97f5714ec07b',
+		name: 'Subscription Report',
+		key: 'report:subscription',
+		description: 'Can view subscription history report',
+		is_internal: false,
+	},
+	{
+		uuid: '32bc6fde-5e46-40f8-94a8-a6c1e42d5d2f',
+		name: 'Transaction Report',
+		key: 'report:transaction',
+		description: 'Can view transaction history report',
+		is_internal: false,
+	},
+	{
+		uuid: 'a724096d-ea34-460f-9e42-4bb2509df3e1',
+		name: 'Create Role',
+		key: 'role:create',
+		description: 'Can create a role data',
+		is_internal: false,
+	},
+	{
+		uuid: 'e015f77e-a8c3-4c3a-b793-f81412840df6',
+		name: 'Edit Role',
+		key: 'role:update',
+		description: 'Can edit a role data',
+		is_internal: false,
+	},
+	{
+		uuid: '4577344b-15f0-432e-b240-597e273f06fb',
+		name: 'Delete Role',
+		key: 'role:delete',
+		description: 'Can delete a role data',
+		is_internal: false,
+	},
+	{
+		uuid: '0d0cbc0a-a0bc-485e-bb62-513f38f6e0f9',
+		name: 'View Role',
+		key: 'role:read',
+		description: 'Can view list of all role data',
+		is_internal: false,
+	},
+	{
+		uuid: '827fa2f8-d9d5-4af6-b20b-ac1dd2845077',
+		name: 'Switch Status Product Outlet',
+		key: 'product_outlet:switch_status',
+		description:
+			'Can switch a status (active or available) product outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: 'b9730bfb-fa63-4e2d-b4b8-36dd60080567',
+		name: 'Edit Product Outlet',
+		key: 'product_outlet:update',
+		description: 'Can edit a product outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: '016f4e95-97c1-43bb-8249-0dd6b65594c7',
+		name: 'View Product Outlet',
+		key: 'product_outlet:read',
+		description: 'Can view list of all product outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: 'ad83b4a0-bd56-4e2c-8369-f77beec51be7',
+		name: 'View Product',
+		key: 'product:read',
+		description: 'Can view list of all product data',
+		is_internal: false,
+	},
+	{
+		uuid: '009c1b0b-e1a5-4af9-8a8d-f0e4b0a31b56',
+		name: 'Delete Product',
+		key: 'product:delete',
+		description: 'Can delete a product data',
+		is_internal: false,
+	},
+	{
+		uuid: '37361975-a0e2-4bad-8237-f28449327d75',
+		name: 'Create Product',
+		key: 'product:create',
+		description: 'Can create a product data',
+		is_internal: false,
+	},
+	{
+		uuid: '58e7227e-7b84-433b-b2d0-4a391aeff675',
+		name: 'Edit Product',
+		key: 'product:update',
+		description: 'Can edit a product data',
+		is_internal: false,
+	},
+	{
+		uuid: 'acc91958-28e9-4841-9118-a3e588a5315e',
+		name: 'Delete Outlet',
+		key: 'restaurant_outlet:delete',
+		description: 'Can delete an outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: 'd5f6eb3f-8881-4e6a-a1ed-ad446f7397a3',
+		name: 'Edit Outlet',
+		key: 'restaurant_outlet:update',
+		description: 'Can edit an outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: '0abc0337-bf03-4ae3-9c6b-09587e336850',
+		name: 'Create Outlet',
+		key: 'restaurant_outlet:create',
+		description: 'Can create an outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: '9d0a9c78-5012-48fb-b04a-42d0c8482c3d',
+		name: 'View Outlet',
+		key: 'restaurant_outlet:read',
+		description: 'Can view list of all outlet data',
+		is_internal: false,
+	},
+	{
+		uuid: '1fb1b92a-a477-4c1c-9198-90165fb41544',
+		name: 'View Restaurant',
+		key: 'restaurant:read',
+		description: 'Can view list of all restaurant data',
+		is_internal: false,
+	},
+	{
+		uuid: 'fbd2c3bf-74e4-4083-a9cf-c296a19c855c',
+		name: 'Delete Restaurant',
+		key: 'restaurant:delete',
+		description: 'Can delete a restaurant data',
+		is_internal: false,
+	},
+	{
+		uuid: '19c7dbfb-1e9a-4b90-89be-79e37705856b',
+		name: 'Edit Restaurant',
+		key: 'restaurant:update',
+		description: 'Can update a restaurant data',
+		is_internal: false,
+	},
+];
+
+const result = access.reduce((acc, curr) => {
+	const [subject, action] = curr.key.split(':');
+	const existingItem = acc.find(item => item.actions === action);
+	if (existingItem) {
+		if (Array.isArray(existingItem.subject)) {
+			existingItem.subject.push(subject);
+		} else {
+			existingItem.subject = [existingItem.subject, subject];
+		}
+	} else {
+		acc.push({
+			actions: action,
+			subject,
+		});
+	}
+	return acc;
+}, []);
+
+// console.log(result);
--- src/view/common/components/templates/sidebar/index.tsx
+++ src/view/common/components/templates/sidebar/index.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-
 import {OutletSelection} from '@/domain/outlet/models';
 import {useLogoutViewModel} from '@/view/auth/view-models/LogoutViewModel';
 import PersonIcon from '@/view/common/assets/icons/person';
@@ -7,6 +6,7 @@ import Logo from '@/view/common/components/atoms/logo';
 import Menu from '@/view/common/components/molecules/menu';
 import SubscriptionReminder from '@/view/common/components/molecules/subscription-reminder';
 import {PROTECT_ROUTES} from '@/view/common/config/link';
+import useDisclosure from '@/view/common/hooks/useDisclosure';
 import useViewportListener from '@/view/common/hooks/useViewportListener';
 import {useAppDispatch, useAppSelector} from '@/view/common/store/hooks';
 import {
@@ -17,11 +17,17 @@ import {
 import {onChangeSelectedTrxId} from '@/view/common/store/slices/transaction';
 import {useGetSubscriptionReminderViewModel} from '@/view/subscription/view-models/GetSubscriptionReminderViewModel';
 import {Select} from 'antd';
+import dynamic from 'next/dynamic';
 import {useRouter} from 'next/router';
+import {Button} from 'posy-fnb-core';
 import React, {useEffect, useState} from 'react';
 import {BsList} from 'react-icons/bs';
 import {FiLogOut} from 'react-icons/fi';
 import {Sidebar, useProSidebar} from 'react-pro-sidebar';
+
+const Modal = dynamic(() => import('posy-fnb-core').then(el => el.Modal), {
+	loading: () => <div />,
+});
 
 type TemplatesSidebarProps = {
 	dataOutletSelection: OutletSelection | undefined;
@@ -31,6 +37,10 @@ const TemplatesSidebar = ({dataOutletSelection}: TemplatesSidebarProps) => {
 	const router = useRouter();
 	const {width} = useViewportListener();
 	const {collapseSidebar, collapsed} = useProSidebar();
+	const [isOpenLogout, {open: openLogout, close: closeLogout}] = useDisclosure({
+		initialState: false,
+	});
+
 	const dispatch = useAppDispatch();
 	const {
 		outletId,
@@ -73,8 +83,9 @@ const TemplatesSidebar = ({dataOutletSelection}: TemplatesSidebarProps) => {
 		dispatch(onChangeSelectedTrxId({id: ''}));
 	};
 
-	const {logout} = useLogoutViewModel({
+	const {logout, isLoading: loadLogout} = useLogoutViewModel({
 		onSuccess: () => {
+			closeLogout();
 			router.push('auth/login');
 			setTimeout(() => {
 				dispatch(onLogout());
@@ -90,80 +101,112 @@ const TemplatesSidebar = ({dataOutletSelection}: TemplatesSidebarProps) => {
 	};
 
 	return (
-		<Sidebar
-			defaultCollapsed={width < 1200}
-			className="relative z-0 h-full overflow-hidden rounded-r-2xl bg-neutral-10"
-			width="250px"
-		>
-			<aside
-				className={`flex h-[12%] w-full items-center transition-all duration-300 ease-in-out sm:justify-start ${
-					collapsed ? 'px-6' : 'pl-12 pt-2'
-				}`}
+		<>
+			<Sidebar
+				defaultCollapsed={width < 1200}
+				className="relative z-0 h-full overflow-hidden rounded-r-2xl bg-neutral-10"
+				width="250px"
 			>
-				{!collapsed ? (
-					<Logo onClick={onCollapse} titleProps="text-xl" />
-				) : (
-					<BsList
-						size={28}
-						onClick={onCollapse}
-						className="fill-primary cursor-pointer"
-					/>
-				)}
-			</aside>
-
-			<aside
-				className={`${
-					dataSubscriptionReminder?.is_show ? 'h-[60%]' : 'h-[70%]'
-				} overflow-y-auto pb-6`}
-			>
-				{PROTECT_ROUTES.map(route => (
-					<Menu key={route.title} item={route} collapse={collapsed} />
-				))}
-			</aside>
-
-			<aside className="absolute bottom-0 w-full items-center">
-				{dataSubscriptionReminder?.is_show && (
-					<SubscriptionReminder end_date={dataSubscriptionReminder?.end_date} />
-				)}
 				<aside
-					className={`w-full ${
-						collapsed ? 'items-center' : 'items-start'
-					} flex h-[140px] flex-col justify-start rounded-t-2xl bg-[#F2F1F9] p-6`}
+					className={`flex h-[12%] w-full items-center transition-all duration-300 ease-in-out sm:justify-start ${
+						collapsed ? 'px-6' : 'pl-12 pt-2'
+					}`}
 				>
-					<div
-						className={`flex items-center gap-2 ${
-							collapsed ? '-ml-1.5 justify-center' : 'justify-start'
-						}`}
-					>
-						<div>
-							<PersonIcon height={24} width={24} />
-						</div>
-						{!collapsed && (
-							<div className="text-m-semibold line-clamp-1">{fullname}</div>
-						)}
-					</div>
-					{dataOutletSelection && (
-						<Select
-							className={`mt-2.5 ${collapsed ? 'w-16' : '!w-[164px]'}`}
-							options={outletOpt}
-							value={outletId}
-							onChange={onChangeOutlet}
-							disabled={!isSubscription}
+					{!collapsed ? (
+						<Logo onClick={onCollapse} titleProps="text-xl" />
+					) : (
+						<BsList
+							size={28}
+							onClick={onCollapse}
+							className="fill-primary cursor-pointer"
 						/>
 					)}
-					<div
-						role="button"
-						onClick={handleLogout}
-						onKeyDown={handleLogout}
-						tabIndex={0}
-						className="mt-2.5 flex cursor-pointer gap-4 hover:opacity-70"
-					>
-						<FiLogOut className="text-neutral-90" size={20} />
-						{!collapsed && <p className="text-m-regular">Logout</p>}
-					</div>
 				</aside>
-			</aside>
-		</Sidebar>
+
+				<aside
+					className={`${
+						dataSubscriptionReminder?.is_show ? 'h-[60%]' : 'h-[70%]'
+					} overflow-y-auto pb-6`}
+				>
+					{PROTECT_ROUTES.map(route => (
+						<Menu key={route.title} item={route} collapse={collapsed} />
+					))}
+				</aside>
+
+				<aside className="absolute bottom-0 w-full items-center">
+					{dataSubscriptionReminder?.is_show && (
+						<SubscriptionReminder
+							end_date={dataSubscriptionReminder?.end_date}
+						/>
+					)}
+					<aside
+						className={`w-full ${
+							collapsed ? 'items-center' : 'items-start'
+						} flex h-[140px] flex-col justify-start rounded-t-2xl bg-[#F2F1F9] p-6`}
+					>
+						<div
+							className={`flex items-center gap-2 ${
+								collapsed ? '-ml-1.5 justify-center' : 'justify-start'
+							}`}
+						>
+							<div>
+								<PersonIcon height={24} width={24} />
+							</div>
+							{!collapsed && (
+								<div className="text-m-semibold line-clamp-1">{fullname}</div>
+							)}
+						</div>
+						{dataOutletSelection && (
+							<Select
+								className={`mt-2.5 ${collapsed ? 'w-16' : '!w-[164px]'}`}
+								options={outletOpt}
+								value={outletId}
+								onChange={onChangeOutlet}
+								disabled={!isSubscription}
+							/>
+						)}
+						<div
+							role="button"
+							onClick={openLogout}
+							onKeyDown={openLogout}
+							className="mt-2.5 flex cursor-pointer gap-4 hover:opacity-70"
+						>
+							<FiLogOut className="text-neutral-90" size={20} />
+							{!collapsed && <p className="text-m-regular">Logout</p>}
+						</div>
+					</aside>
+				</aside>
+			</Sidebar>
+			<Modal open={isOpenLogout} closeOverlay handleClose={closeLogout}>
+				<section className="flex w-[380px] flex-col items-center justify-center p-4">
+					<div className="px-16">
+						<p className="text-center text-l-semibold line-clamp-2">
+							Are you sure you want to logout?
+						</p>
+					</div>
+					<div className="mt-8 flex w-full gap-3">
+						<Button
+							variant="secondary"
+							size="l"
+							fullWidth
+							onClick={closeLogout}
+							className="whitespace-nowrap"
+						>
+							Cancel
+						</Button>
+						<Button
+							isLoading={loadLogout}
+							variant="primary"
+							size="l"
+							fullWidth
+							onClick={handleLogout}
+						>
+							Logout
+						</Button>
+					</div>
+				</section>
+			</Modal>
+		</>
 	);
 };
 
