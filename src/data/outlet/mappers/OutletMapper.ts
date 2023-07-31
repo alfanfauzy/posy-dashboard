1520c97a061dda01e1487212e0c4a1a75421ed9a
alfanfauzy <zhafhy@gmail.com>
2023-07-31 17:11:40 +0700

    fix(code): remove unused code

--- src/data/outlet/mappers/OutletMapper.ts
+++ src/data/outlet/mappers/OutletMapper.ts
@@ -1,9 +1,7 @@
 import {OutletSelection} from '@/domain/outlet/models';
-import {OutletDetail} from '@/domain/outlet/models/GetOutletDetailModel';
 import {OutlerUsers} from '@/domain/outlet/repositories/GetOutletUserRepository';
 
 import {GetOutletSelectionDataResponse} from '../types';
-import {GetOutletDetailResponse} from '../types/GetOutletDetailType';
 import {GetOutletUserDataResponse} from '../types/GetOutletUserType';
 
 export const mapToOutletSelectionModel = (
@@ -24,36 +22,3 @@ export const mapToOutletUserModel = (
 		full_name: data.fullname,
 		phone: data.phone,
 	}));
-
-export const mapToOutletDetailModel = (
-	datas: GetOutletDetailResponse,
-): OutletDetail => ({
-	address: datas.outlet.address,
-	email: datas.outlet.email,
-	latitude: datas.outlet.latitude,
-	logo_image_url: datas.outlet.logo_image_url,
-	longitude: datas.outlet.longitude,
-	uuid: datas.outlet.uuid,
-	restaurant_uuid: datas.outlet.restaurant_uuid,
-	restaurant_code: datas.outlet.restaurant_code,
-	restaurant_name: datas.outlet.restaurant_name,
-	restaurant_email: datas.outlet.restaurant_email,
-	outlet_name: datas.outlet.outlet_name,
-	outlet_code: datas.outlet.outlet_code,
-	region: {
-		province_id: datas.outlet.region.province_id,
-		province_name: datas.outlet.region.province_name,
-		city_id: datas.outlet.region.city_id,
-		city_name: datas.outlet.region.city_name,
-		district_id: datas.outlet.region.district_id,
-		district_name: datas.outlet.region.district_name,
-		subdistrict_id: datas.outlet.region.subdistrict_id,
-		subdistrict_name: datas.outlet.region.subdistrict_name,
-		postal_code: datas.outlet.region.postal_code,
-	},
-	phone: datas.outlet.phone,
-	metadata: {
-		created_at: datas.outlet.metadata.created_at.nanos,
-		updated_at: datas.outlet.metadata.created_at.nanos,
-	},
-});
--- src/domain/outlet/repositories/GetOutletDetailRepository.ts
+++
@@ -1,9 +0,0 @@
-import {ResultQuery} from '@/domain/vo/BaseResponse';
-
-import {OutletDetail} from '../models/GetOutletDetailModel';
-
-export type GetOutletDetailPayload = {
-	restaurant_outlet_uuid: string;
-};
-
-export type GetOutletDetailResult = ResultQuery<OutletDetail | undefined>;
