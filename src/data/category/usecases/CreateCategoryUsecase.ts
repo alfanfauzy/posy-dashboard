edf70d905349cbcfd58d1dd4181e01dceca9b705
zaidanammar <zaidanammar23@gmail.com>
2023-07-31 10:57:18 +0700

    fix: takeout unused code :fire:

--- src/data/category/usecases/CreateCategoryUsecase.ts
+++ src/data/category/usecases/CreateCategoryUsecase.ts
@@ -35,10 +35,6 @@ export const useCreateCategoryUsecase = ({
 			if (dataError) {
 				const err = mapToBaseError(dataError);
 				onError?.(err, ...args);
-				enqueueSnackbar({
-					message: err.message,
-					variant: 'error',
-				});
 			}
 		},
 		...options,
